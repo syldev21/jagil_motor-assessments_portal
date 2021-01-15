6719c0f8ffe443862f091f97d1b48dda8d807154
Stephen Ondieki <Stephen.Ondieki@jubileekenya.com>
2021-01-15 10:39:12 +0300

    Updating kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        'App\Console\Commands\ResizeImages'
+        \App\Console\Commands\ResizeImages::class
     ];
 
     /**
