7597cca7462fcd2b844782659710bcc54f1fc7f7
Stephen Ondieki <Stephen.Ondieki@jubileekenya.com>
2021-02-25 09:18:03 +0300

    Add filter to head assessor assessments

--- app/Http/Controllers/HeadAssessorController.php
+++ app/Http/Controllers/HeadAssessorController.php
@@ -232,12 +232,28 @@ class HeadAssessorController extends Controller
     public function assessments(Request $request)
     {
         try {
-//            $segmentIds = array(Config::$ASSESSMENT_SEGMENTS['ASSESSMENT']['ID'], Config::$ASSESSMENT_SEGMENTS['RE_INSPECTION']['ID']);
             $assessmentStatusID = $request->assessmentStatusID;
-            $assessments = Assessment::where(["assessmentStatusID" => $assessmentStatusID])
-//                ->whereIn('segment', $segmentIds)
-                ->where('segment',"!=",Config::$ASSESSMENT_SEGMENTS['SUPPLEMENTARY']['ID'])
-                ->orderBy('dateCreated', 'DESC')->with('claim')->with('approver')->with('final_approver')->with('assessor')->with('supplementaries')->get();
+            if (!isset($request->fromDate) && !isset($request->toDate) && !isset($request->regNumber)) {
+                $assessments = Assessment::where(["assessmentStatusID" => $assessmentStatusID])
+                    ->where('segment', "!=", Config::$ASSESSMENT_SEGMENTS['SUPPLEMENTARY']['ID'])
+                    ->where('dateCreated', ">=", Carbon::now()->subDays(Config::DATE_RANGE))
+                    ->orderBy('dateCreated', 'DESC')->with('claim')->with('approver')->with('final_approver')->with('assessor')->with('supplementaries')->get();
+            } elseif (isset($request->regNumber)) {
+                $claimids = Claim::where('vehicleRegNo','like', '%'.$request->regNumber.'%')->pluck('id')->toArray();
+                $assessments = Assessment::where(["assessmentStatusID" => $assessmentStatusID])
+                    ->where('segment', "!=", Config::$ASSESSMENT_SEGMENTS['SUPPLEMENTARY']['ID'])
+                    ->whereIn('claimID', $claimids)
+                    ->orderBy('dateCreated', 'DESC')->with('claim')->with('approver')->with('final_approver')->with('assessor')->with('supplementaries')->get();
+            } elseif (isset($request->fromDate) && isset($request->toDate) && !isset($request->regNumber)) {
+                $fromDate = Carbon::parse($request->fromDate)->format('Y-m-d H:i:s');
+                $toDate = Carbon::parse($request->toDate)->format('Y-m-d H:i:s');
+                $assessments = Assessment::where(["assessmentStatusID" => $assessmentStatusID])
+                    ->where('segment', "!=", Config::$ASSESSMENT_SEGMENTS['SUPPLEMENTARY']['ID'])
+                    ->whereBetween('dateCreated', [$fromDate, $toDate])
+                    ->orderBy('dateCreated', 'DESC')->with('claim')->with('approver')->with('final_approver')->with('assessor')->with('supplementaries')->get();
+            } else {
+                $assessments = array();
+            }
             return view('head-assessor.assessments', ["assessments" => $assessments, 'assessmentStatusID' => $assessmentStatusID]);
         } catch (\Exception $e) {
             $this->log->motorAssessmentInfoLogger->info("FUNCTION " . __METHOD__ . " " . " LINE " . __LINE__ .
--- public/js/script.js
+++ public/js/script.js
@@ -1375,6 +1375,47 @@ $(document).ready(function () {
 
             success: function (data) {
                 $("#main").html(data);
+                $('.datepicker').datepicker();
+                $('#data-table-simple').DataTable({
+                    dom: 'Bfrtip',
+                    buttons: [
+                        'copy', 'csv', 'excel', 'pdf', 'print'
+                    ],
+                    "pageLength": 25
+                });
+            }
+
+        });
+    });
+    $("#main").on('click','#filter-head-assessor-assessments',function (e){
+        e.preventDefault();
+        var assessmentStatusID = $("#assessmentStatusID").val();
+        var fromDate = $("#from_date").val();
+        var toDate = $("#to_date").val();
+        var regNumber = $("#vehicle_reg_no").val();
+        $.ajaxSetup({
+
+            headers: {
+
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+
+            }
+
+        });
+        $.ajax({
+
+            type: 'POST',
+            data : {
+                assessmentStatusID : assessmentStatusID,
+                fromDate : fromDate,
+                toDate : toDate,
+                regNumber : regNumber
+            },
+            url: '/head-assessor/assessments',
+
+            success: function (data) {
+                $("#main").html(data);
+                $('.datepicker').datepicker();
                 $('#data-table-simple').DataTable({
                     dom: 'Bfrtip',
                     buttons: [
