b151cf2899c37145d03cb3364db5be5811441dd5
Stephen Ondieki <Stephen.Ondieki@jubileekenya.com>
2021-04-16 15:19:18 +0300

    Enabling the functionality for full screen on the portal

--- public/js/script.js
+++ public/js/script.js
@@ -3,6 +3,9 @@ const GENERIC_ERROR_CODE = 4000;
 const NO_RECORDS_FOUND = 3000;
 $(document).ready(function () {
     $(".sidenav").sidenav();
+    $(".toggle-fullscreen").click(function() {
+        document.fullScreenElement && null !== document.fullScreenElement || !document.mozFullScreen && !document.webkitIsFullScreen ? document.documentElement.requestFullScreen ? document.documentElement.requestFullScreen() : document.documentElement.mozRequestFullScreen ? document.documentElement.mozRequestFullScreen() : document.documentElement.webkitRequestFullScreen ? document.documentElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT) : document.documentElement.msRequestFullscreen && (document.msFullscreenElement ? document.msExitFullscreen() : document.documentElement.msRequestFullscreen()) : document.cancelFullScreen ? document.cancelFullScreen() : document.mozCancelFullScreen ? document.mozCancelFullScreen() : document.webkitCancelFullScreen && document.webkitCancelFullScreen();
+    });
     $('.materialboxed').materialbox();
     $("#login-btn").on('click', function (e) {
         e.preventDefault();
