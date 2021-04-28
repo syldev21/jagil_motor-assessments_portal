34127ffd9f3bb2f09114f8915ea3d7b6d7c4dc61
Stephen Ondieki <Stephen.Ondieki@jubileekenya.com>
2021-04-28 12:24:15 +0300

    Making modification to adjuster assessment report

--- resources/views/adjuster/assessment-report.blade.php
+++ resources/views/adjuster/assessment-report.blade.php
@@ -339,6 +339,13 @@
                                         @endforeach
 
                                         @if($assessment['assessmentTypeID'] != 2)
+                                            <?php
+                                            if (isset($assessment['totalChange']) && isset($priceChange->finalApprovedAt)) {
+                                                $totalCostSubjected = $assessment['totalChange'];
+                                            } else {
+                                                $totalCostSubjected = $assessment['totalCost'];
+                                            }
+                                            ?>
 
                                             @if($assessment['claim']->intimationDate >= \App\Conf\Config::VAT_REDUCTION_DATE && $assessment['claim']->intimationDate <= \App\Conf\Config::VAT_END_DATE)
                                                 <tr>
@@ -348,7 +355,7 @@
                                                     <td class="text-bold">Sum Total</td>
                                                     <td></td>
                                                     <td></td>
-                                                    <td>{{ number_format(($assessment['totalCost']) - round(($assessment['totalCost']*\App\Conf\Config::CURRENT_VAT)/\App\Conf\Config::CURRENT_TOTAL_PERCENTAGE)) }}</td>
+                                                    <td>{{ number_format(($totalCostSubjected) - round(($totalCostSubjected*\App\Conf\Config::CURRENT_VAT)/\App\Conf\Config::CURRENT_TOTAL_PERCENTAGE)) }}</td>
                                                     <td></td>
                                                 </tr>
 
@@ -361,7 +368,7 @@
                                                     </td>
                                                     <td></td>
                                                     <td></td>
-                                                    <td>{{ number_format(round(($assessment['totalCost']*App\Conf\Config::CURRENT_VAT)/\App\Conf\Config::CURRENT_TOTAL_PERCENTAGE)) }}</td>
+                                                    <td>{{ number_format(round(($totalCostSubjected*App\Conf\Config::CURRENT_VAT)/\App\Conf\Config::CURRENT_TOTAL_PERCENTAGE)) }}</td>
                                                     <td></td>
                                                 </tr>
                                             @else
@@ -372,7 +379,7 @@
                                                     <td class="text-bold">Sum Total</td>
                                                     <td></td>
                                                     <td></td>
-                                                    <td>{{ number_format(($assessment['totalCost']) - round(($assessment['totalCost']*\App\Conf\Config::VAT)/\App\Conf\Config::TOTAL_PERCENTAGE)) }}</td>
+                                                    <td>{{ number_format(($totalCostSubjected) - round(($totalCostSubjected*\App\Conf\Config::VAT)/\App\Conf\Config::TOTAL_PERCENTAGE)) }}</td>
                                                     <td></td>
                                                 </tr>
 
@@ -383,7 +390,7 @@
                                                     <td class="">{{\App\Conf\Config::VAT_PERCENTAGE}} VAT</td>
                                                     <td></td>
                                                     <td></td>
-                                                    <td>{{ number_format(round(($assessment['totalCost']*\App\Conf\Config::VAT)/\App\Conf\Config::TOTAL_PERCENTAGE)) }}</td>
+                                                    <td>{{ number_format(round(($totalCostSubjected*\App\Conf\Config::VAT)/\App\Conf\Config::TOTAL_PERCENTAGE)) }}</td>
                                                     <td></td>
                                                 </tr>
                                             @endif
