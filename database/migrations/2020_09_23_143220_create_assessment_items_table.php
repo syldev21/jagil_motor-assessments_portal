e5e5cdbd5294af352c75b9654c1e86fadf9491c5
Stephen Ondieki <Stephen.Ondieki@jubileekenya.com>
2021-04-30 12:42:13 +0300

    Adding a column for totalDifference

--- database/migrations/2020_09_23_143220_create_assessment_items_table.php
+++ database/migrations/2020_09_23_143220_create_assessment_items_table.php
@@ -23,6 +23,7 @@ class CreateAssessmentItemsTable extends Migration
             $table->double('cost')->nullable();
             $table->double('current')->nullable();
             $table->double('difference')->nullable();
+            $table->double('totalDifference')->nullable();
             $table->double('total')->nullable();
             $table->longText('remarks')->nullable();
             $table->integer('assessmentItemType')->nullable();
