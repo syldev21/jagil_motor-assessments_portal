a8b8cbbdfbbb08f93af696ca7cdf95760c528209
Stephen Ondieki <Stephen.Ondieki@jubileekenya.com>
2021-06-22 17:10:19 +0300

    Adding a model for companies

---
+++ app/Company.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Company extends Model
+{
+    protected $connection = "mysql";
+    protected $table = "companies";
+    protected $primaryKey = "id";
+    protected $fillable = ['name','building','street','city','updatedBy','createdBy','dateModified','dateCreated'];
+    public $timestamps= false;
+}
---
+++ database/migrations/2021_06_22_161121_create_companies_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name')->nullable();
+            $table->string('building')->nullable();
+            $table->string('street')->nullable();
+            $table->string('city')->nullable();
+            $table->bigInteger('updatedBy')->nullable();
+            $table->bigInteger('createdBy')->nullable();
+            $table->dateTime('dateModified')->nullable();
+            $table->timestamp('dateCreated')->default(DB::raw('CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+}
