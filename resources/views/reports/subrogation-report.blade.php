1b8665c862cb2b7154e69be777f4d88e3f1bbd8f
Sylvester Ouma <Sylvester.Ouma@jubileekenya.com>
2022-04-25 16:28:02 +0300

    adding send subrogation

--- resources/views/reports/subrogation-report.blade.php
+++ resources/views/reports/subrogation-report.blade.php
@@ -1,62 +1,91 @@
-<!DOCTYPE html>
-<html lang="en">
+@include('_partials.header')
+<div id="main">
+    <div class="row">
+        <div class="col s12">
+            <div class="container">
+                <div class="row">
+                    <div class="col s12">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>SUBROGATION REPORT</title>
-    <style>
+                        <div class="divider"></div>
+                        <div class="row">
+                            <div class="col s4">
 
+                            </div>
+                            <div class="col s4">
+                                <img class="responsive-img" src="{{url('images/logo/jubilee_logo.png') }}">
+                            </div>
+                            <div class="col s4">
 
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col s12">
+                                <h5 style="font-weight: bold;">“WITHOUT PREJUDICE”</h5><br/>
+                                <h6><span style="font-weight: bold;">OUR REF: </span>{{$claim->claimNo}}</h6>
+                                <h6><span style="font-weight: bold;">YOUR REF: </span>POL
+                                    NO: {{$claim->thirdPartyPolicy }}</h6>
+                                <br/>
+                                <p>
+                                    <?php
+                                    echo date('d/m/Y');
+                                    ?>
+                                </p>
+                                <br/>
+                                <h5>{{$company->name}}</h5>
+                                <h5>{{$company->building}}</h5>
+                                <h5>{{$company->stricty}}</h5>
+                                <h5>{{$company->city}}.</h5>
+                                <br/>
+                                <h6>Dear Sirs,</h6>
+                                <br/>
+                                <div style="text-decoration: underline;">
+                                    <h5>ACCIDENT ON {{$claim->loseDate}}</h5>
+                                    <h5>YOUR INSURED/INSURED’S DRIVER: {{$claim->thirdPartyDriver}} REG
+                                        NO. {{$claim->thirdPartyVehicleRegNo}}</h5>
+                                    <h5>OUR INSURED: {{$claim->customer->fullName}} REG.
+                                        NO. {{$claim->vehicleRegNo}}</h5>
+                                </div>
 
-    </style>
-</head>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col s12">
+                                <p style="line-height: 200%">
+                                    We refer to the above accident. We presume by now your insured has reported
+                                    the same to you. Your insured’s driver was the cause of this accident and
+                                    the police blamed him for the same.
+                                </p>
+                                <p style="line-height: 200%">
+                                    We hold you liable for the loss incurred as a result of the said accident
+                                    which we expended sums in making good the damages suffered by our insured to
+                                    the tune of Ksh.
+                                    @if(isset($assessment->totalCost))
+                                        {{isset($assessment->totalChange) ? number_format($assessment->totalChange) : number_format($assessment->totalCost) }}
+                                    @elseif(isset($assessment->totalLoss))
+                                        {{isset($assessment->totalChange) ? number_format($assessment->totalChange) : number_format($assessment->totalCost) }}
+                                    @endif
 
-<body>
-<div class="">
-    <h4 class="">Subrogation Report</h4>
-</div>
+                                    /-
+                                </p>
+                                <p style="line-height: 200%">
+                                    We hold you liable for the loss we suffered in making good damages sustained
+                                    by our insured. Kindly let us have your admission of liability within 21
+                                    days from the date hereof.
+                                </p>
+                                <p style="line-height: 200%">
+                                    In default we shall assume you are not interested in an amicable settlement
+                                    and continue to advise our lawyers to serve your insured with summons.
+                                </p>
 
-<div class="row">
-    <div class="">
-
+                                <h5>Yours faithfully,</h5>
+                                <img src="{{'images/e_signatures/'.auth()->user()->signature }}" width="10%">
+                                <h5>{{auth()->user()->name}}</h5>
+                                <h5>NON-LIFE CLAIMS</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-    <div class="">
-        <img class="" src="{{url('images/logo/jubilee_logo.png') }}">
-    </div>
-    <div class="">
-
-    </div>
 </div>
-
-<div>
-
-    <p>“WITHOUT PREJUDICE”</p>
-    <p>OUR REF: C/102/1002/2022/000018</p>
-    <p>YOUR REF: POL NO: POL NO: {{$claim->thirdPartyPolicy }}</p>
-
-    <p>22/04/2022</p>
-
-    <p>Monarch Insurance Co Ltd</p>
-    <p>Monarch House</p>
-    <p>Nairobi.</p>
-
-    <p> Dear Sirs,</p>
-
-    <h4><u>ACCIDENT ON 2022-03-31 00:00:00</u></h4>
-    <h4><u>YOUR INSURED/INSURED’S DRIVER: BENSON EVANS MACHOGE REG NO. KBU 942Y</u></h4>
-    <h4><u>OUR INSURED: DR. EMMANUEL ALENGA AMADI REG. NO. KCG 538C</u></h4>
-    <br>
-
-    <p> We refer to the above accident. We presume by now your insured has reported the same to you. Your insured’s driver was the cause of this accident and the police blamed him for the same.</p>
-
-    <p>We hold you liable for the loss incurred as a result of the said accident which we expended sums in making good the damages suffered by our insured to the tune of Ksh. 91,400 /-</p>
-
-    <p>We hold you liable for the loss we suffered in making good damages sustained by our insured. Kindly let us have your admission of liability within 21 days from the date hereof.</p>
-
-    <p> In default we shall assume you are not interested in an amicable settlement and continue to advise our lawyers to serve your insured with summons.</p>
-    <p>Yours faithfully,</p>
-</div>
-</body>
-</html>
