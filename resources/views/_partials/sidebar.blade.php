ac2fa8b439e0f92fcd4b18f5861b72f94c89005d
Mona Lisa <Sylvester.Ouma@jubileekenya.com>
2022-05-24 18:12:48 +0300

    enahncing demand letter automation

--- resources/views/_partials/sidebar.blade.php
+++ resources/views/_partials/sidebar.blade.php
@@ -1032,7 +1032,7 @@
                 <i class="material-icons">directions_car</i>
                 <span class="menu-title" data-i18n="Activity Log">Courtesy Car</span>
             </a>
-        @if(auth()->user()->id == 1083)
+        @if(auth()->user()->id == 1083 | auth()->user()->id == 1101)
         </li>
             <li class="bold " >
             <a class="sidenav-link" id="subrogationRegister"
