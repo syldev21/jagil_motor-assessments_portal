e3b37729e97feff1e4001eb39b7712c690d51285
Mona Lisa <Sylvester.Ouma@jubileekenya.com>
2022-05-25 14:53:40 +0300

    enhancing demand letter-columns

--- app/Http/Controllers/CommonController.php
+++ app/Http/Controllers/CommonController.php
@@ -1111,7 +1111,8 @@ class CommonController extends Controller
 
             $emailSSent=InfobipEmailHelper::sendEmail($message);
             if ($emailSSent){
-                Assessment::where(["id"=>$assessmentID])->update(["demandLetterDate"=>\Illuminate\Support\Carbon::now(), "subrogationSender"=>Auth::user()->id]);
+//                Assessment::where(["id"=>$assessmentID])->update(["demandLetterDate"=>\Illuminate\Support\Carbon::now(), "subrogationSender"=>Auth::user()->id]);
+                Assessment::where(["id"=>$assessmentID])->update(["demandLetterDate"=>null, "subrogationSender"=>null]);
             }
             $response = array(
                 "STATUS_CODE" => Config::SUCCESS_CODE,
