01cd53850a1412ce09c18ed9b6d5e9e556f35a04
Mona Lisa <Sylvester.Ouma@jubileekenya.com>
2022-06-20 17:01:26 +0300

    adding sfaricom customer detail view

---
+++ resources/views/safaricom-home-fibre/customer/portfolio.blade.php
@@ -0,0 +1,119 @@
+<div class="row">
+
+    <div
+        class="content-wrapper-before  gradient-45deg-red-pink">
+    </div>
+    <div class="col s12">
+        <div class="container">
+            <div class="row">
+                <div class="col s12">
+                    <div class="card">
+                        <div class="card-content">
+                            <div class="row">
+                                <h4 class="card-title float-left">
+                                    My Portfolio
+                                </h4>
+
+                            </div>
+                            <div class="row">
+                                <div class="row">
+                                    <div class="input-field col m3 s6">
+                                        <i class="material-icons prefix">access_time</i>
+                                        <input id="from_date" type="text" class="validate datepicker">
+                                        <label for="from_date">From Date</label>
+                                    </div>
+                                    <div class="input-field col m3 s6">
+                                        <i class="material-icons prefix">access_time</i>
+                                        <input id="to_date" type="text" class="validate datepicker">
+                                        <label for="to_date">To Date</label>
+                                    </div>
+                                    <div class="input-field col m3 s6">
+                                        <i class="material-icons prefix">vpn_key</i>
+                                        <input id="claimNo" type="text" class="validate">
+                                        <input id="claimStatusID" type="hidden" value="">
+                                        <label for="claimNo">Claim No</label>
+                                    </div>
+                                    <div class="input-field col m3 s12">
+                                        <div class="input-field col s12">
+                                            <button class="btn cyan waves-effect waves-light" type="submit" id="filter_nhif_claims"
+                                                    name="action">
+                                                <i class="material-icons left">search</i> Filter
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="divider"></div>
+                            <div class="row ">
+                                <div class="col s12 ">
+                                    <table id="data-table-simple" class="display">
+                                        <thead>
+                                        <tr>
+                                            <th>S/N</th>
+                                            <th>Product Name</th>
+                                            <th>Policy Number</th>
+                                            <th>Policy From Date</th>
+                                            <th>Policy To Date</th>
+                                            <th>Cause of Loss</th>
+                                            <th>Sum Insured</th>
+                                            <th>Premium</th>
+                                            <th>Status</th>
+                                            <th>Operations</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        <?php $claims= array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);?>
+                                        @foreach($claims as $claim)
+                                            <tr>
+                                                <td>{{$loop->iteration}}</td>
+
+                                                <td>
+
+                                                    <a href="#" class="nhifClaimDetails" data-id="" id=""></a>
+                                                </td>
+
+                                                <td></td>
+                                                <td></td>
+                                                <td></td>
+                                                <td></td>
+                                                <td></td>
+                                                <td></td>
+                                                <td></td>
+                                                <td>
+                                                    <!-- Dropdown Trigger -->
+                                                    <a class='dropdown-trigger' href='#'
+{{--                                                       data-target='{{$loop->iteration}}'--}}
+{{--                                                       data-activates="{{$loop->iteration}}">--}}
+                                                        data-target=''
+                                                       data-activates="">
+                                                        <i
+                                                            class="Medium material-icons">menu</i><i
+                                                            class="Medium material-icons">expand_more
+                                                        </i>
+                                                    </a>
+
+                                                    <!-- Dropdown Structure -->
+
+                                                    <ul id='' class='dropdown-content'>
+{{--                                                    <ul id='{{$loop->iteration}}' class='dropdown-content'>--}}
+
+                                                        <li>
+                                                            <a href="#" id="uploadDocumentsForm" data-id="" ><i
+                                                                    class="material-icons">upload</i>Upload Document</a>
+                                                        </li>
+                                                    </ul>
+                                                </td>
+
+                                            </tr>
+                                        @endforeach
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
