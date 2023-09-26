34cf9f705d8f626e82af75760f34799ac13cb59b
Sylvester Ouma <Sylvester.Ouma@allianz.com>
2023-09-26 09:36:30 +0300

    comment the error dumping line

--- app/Http/Controllers/AssessorController.php
+++ app/Http/Controllers/AssessorController.php
@@ -363,7 +363,7 @@ class AssessorController extends Controller
                 "STATUS_MESSAGE" => "Congratulations! Your documents have been uploaded successfully"
             ];
         } catch (\Exception $e) {
-            dd($e->getMessage());
+            // dd($e->getMessage());
             $response = [
                 "STATUS_CODE" => Config::GENERIC_ERROR_CODE,
                 "STATUS_MESSAGE" => Config::GENERIC_ERROR_MESSAGE
